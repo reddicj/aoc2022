b6b66a6cf9f8c7f03201f36f62054cc44e260969
James Reddick <reddicj@gmail.com>
2022-12-02 08:07:52 +0000

    Change

--- Day01/Day01.scala
+++ Day01/Day01.scala
@@ -1,28 +1,20 @@
 package day01
 
-import scala.annotation.tailrec
-
 @main def main =
 
-  val data: List[List[Int]] = read(Input.data)
+  val data: List[List[Int]] =
+    Input.data.trim
+      .split("\n\n")
+      .toList
+      .map(
+        _.trim
+          .split("\n")
+          .toList
+          .map(_.toInt)
+      )
 
   val max: Int = data.map(_.sum).max
   println(s"Max: $max")
 
   val topThreeTotal: Int = data.map(_.sum).sorted.takeRight(3).sum
   println(s"Top 3 total: $topThreeTotal")
-
-end main
-
-def read(input: String): List[List[Int]] =
-  group(input.trim.split("\n").toList, "").map(_.map(_.toInt))
-
-def group[A](as: List[A], delimiter: A): List[List[A]] =
-  @tailrec
-  def run(remaining: List[A], acc: List[List[A]]): List[List[A]] =
-    if remaining.isEmpty then acc
-    else
-      val g      = remaining.takeWhile(_ != delimiter)
-      val update = if g.nonEmpty then g :: acc else acc
-      run(remaining.drop(g.size + 1), update)
-  run(as, Nil).reverse
